c04d8fee94ee501e66c01eca90d9d4411f23bf0a
Cyril Strone <77802640+CyrilStrone@users.noreply.github.com>
2026-04-18 01:19:19 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# 'library-name'
+
+This is a template for functional typescript library from Jenesei Software.
+
+Replace ``library-name`` with the name of your library.
+
+## Peer Dependencies
---
+++ vite.config.ts
@@ -0,0 +1,46 @@
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+import path, { resolve } from 'node:path';
+
+export default defineConfig(() => {
+  return {
+    resolve: {
+      alias: {
+        '@local': path.resolve(__dirname, './src'),
+      },
+    },
+    plugins: [
+      dts({
+        tsconfigPath: './tsconfig.json',
+        outDir: './build',
+        entryRoot: './src',
+        compilerOptions: {
+          rootDir: './src'
+        },
+        insertTypesEntry: true,
+        logLevel: 'info'
+      })
+    ],
+    publicDir: false,
+    build: {
+      sourcemap: true,
+      outDir: './build',
+      rootDir: './src',
+      lib: {
+        entry: {
+          index: resolve(__dirname, 'src/index.ts'),
+        },
+        formats: ['es', 'cjs'],
+        fileName: (format, name) => `${name}.${format}.js`,
+      },
+      rollupOptions: {
+        // Add external dependencies that shouldn't be bundled into the library
+        external: [],
+        output: {
+          globals: {},
+        },
+      },
+    },
+  };
+});
